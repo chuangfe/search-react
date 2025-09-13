a3feee3e19e37ef92c703e1ddb7b7c682a366de2
chuangfe <chuangfe@gmail.com>
2025-09-14 00:29:29 +0800

    add messages constants

---
+++ src/constants/messages.ts
@@ -0,0 +1,14 @@
+export const SERVER_ERROR_MESSAGE = {
+  retry: '伺服器錯誤, 請再次嘗試',
+  wait: '伺服器錯誤, 請稍後再試'
+};
+
+export const ABORT_MESSAGES = {
+  api: '中斷 api',
+  retry: '中斷 retry',
+  deabunce: '中斷 deabunce'
+};
+
+export const DEABUNCE_MESSAGES = {
+  reset: '重置 deabunce timer'
+};
